6eb2ad154333604152988ebc003effd10394a684
Indie Gabo <indiegabodev@gmail.com>
2024-02-05 15:40:44 -0300

    Just testing CI

---
+++ Runtime/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using HandyTools;
+
+namespace MainProject
+{
+    public class Test : HandyBehaviour
+    {
+        #region Inspector
+
+        #endregion
+
+        #region Behaviour
+
+        #endregion
+    }
+}
