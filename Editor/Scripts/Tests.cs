4eaf73a10d37fbb56b41ee064993a3b27c76ebd9
Indie Gabo <indiegabodev@gmail.com>
2024-02-05 23:07:56 -0300

    Tests.cs

--- Editor/Scripts/Tests.cs
+++ Editor/Scripts/Tests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Lungfecther.Web;
 using Lungfetcher.Editor;
 using UnityEditor;
@@ -38,6 +40,46 @@ public class MyEditorWindow : EditorWindow
         {
             Debug.Log(response.HttpErrorMessage);
         }
+    }
 
+    public async Task FetchAll()
+    {
+        // Exibir ui de carregamento do processo todo
+
+        List<Task> tasks = new();
+
+        // foreach (var table in LungSettings.instance.projects[0].tables)
+        // {
+        //     tasks.Add(FetchTable(table.id));
+        // }
+
+        await Task.WhenAll(tasks);
+
+        // remover ui de carregamento
+    }
+
+
+    public async Task FetchTable(int id)
+    {
+        // Exibir ui de carregamento
+
+        var request = WebRequest.To("http://localhost/api/v0/external/tables/87/entries");
+
+        request.SetBearerAuth("QcS74OK.M4L77werD9BhsrGGPjixUgKwjVmFrfXQ");
+
+        WebResponse response = await request.SendAsync();
+
+        if (response.Success)
+        {
+            Debug.Log(response.Contents);
+        }
+        else
+        {
+            Debug.Log(response.HttpErrorMessage);
+        }
+
+        // Traduzir as entradas em unity localization string table
+
+        // Remover ui de carregamento
     }
 }
